5a0c26f1f9133cbc75c472622604013693923e15
chandra-pi <chandrahyd.45@gmail.com>
2026-08-10 19:13:03 +0530

    common instance

--- variables.tf
+++ variables.tf
@@ -46,3 +46,38 @@ variable "database_subnet_tags" {
     type = map(string)
     default = {}
 }
+
+variable "eip_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "is_peering_required" {
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    type = map(string)
+    default = {}
+}
+
